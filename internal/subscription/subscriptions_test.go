e854f65324e34adc4bfa4ead4a457c09c6654f40
Peter SZTANOJEV <sztanpet@gmail.com>
2015-02-17 16:30:12 +0100

    subscriptions: beef up tests a little

--- internal/subscription/subscriptions_test.go
+++ internal/subscription/subscriptions_test.go
@@ -6,6 +6,11 @@ import (
 	"time"
 )
 
+type StartEnd struct {
+	startt time.Time
+	endt   time.Time
+}
+
 func getDurationUpTo(rows []*Row, index int) time.Duration {
 	var sum time.Duration
 	for i, j := 1, len(rows); i < j; i++ {
@@ -20,41 +25,68 @@ func getDurationUpTo(rows []*Row, index int) time.Duration {
 	return sum
 }
 
-func TestFixupSubTime(t *testing.T) {
-	now := time.Now().UTC().Round(time.Second)
-	data := []*Row{
-		{
-			Starttimestamp: now.Add(-(time.Hour * 24 * 7 * 2)),
-			Endtimestamp:   now.Add(-(time.Hour * 24 * 7 * 2)).AddDate(0, 1, 0),
-		},
-		{
-			Starttimestamp: now,
-			Endtimestamp:   now.AddDate(0, 1, 0),
-		},
-		{
-			Starttimestamp: now.AddDate(0, 1, 0),
-			Endtimestamp:   now.AddDate(0, 2, 0),
-		},
-		{
-			Starttimestamp: now.AddDate(0, 2, 0),
-			Endtimestamp:   now.AddDate(0, 3, 0),
-		},
-	}
-	expected := []time.Time{
-		data[0].Endtimestamp,
-		data[0].Endtimestamp.Add(getDurationUpTo(data, 1)),
-		data[0].Endtimestamp.Add(getDurationUpTo(data, 2)),
-		data[0].Endtimestamp.Add(getDurationUpTo(data, 3)),
-	}
-
-	fixupSubTime(data)
+func checkStartEndTimes(prefix string, data []*Row, expected []StartEnd, t *testing.T) {
 	for k, v := range data {
-		if !v.Endtimestamp.Equal(expected[k]) {
-			t.Errorf("Expected date %+v, got %+v at index %v", expected[k], v.Endtimestamp, k)
+		if !v.Starttimestamp.Equal(expected[k].startt) {
+			t.Errorf(
+				"%v Expected startt date %+v, got %+v at index %v",
+				prefix, expected[k].startt, v.Starttimestamp, k,
+			)
+		}
+
+		if !v.Endtimestamp.Equal(expected[k].endt) {
+			t.Errorf(
+				"%v Expected end date %+v, got %+v at index %v",
+				prefix, expected[k].endt, v.Endtimestamp, k,
+			)
 		}
 	}
+}
 
-	data = []*Row{
+func TestFixupSubTime(t *testing.T) {
+	now := time.Now().UTC().Round(time.Second)
+
+	data := []*Row{ // inserting at the second position
+		{
+			Starttimestamp: now.Add(-(time.Hour * 24 * 7 * 2)),
+			Endtimestamp:   now.Add(-(time.Hour * 24 * 7 * 2)).AddDate(0, 1, 0),
+		},
+		{
+			Starttimestamp: now,
+			Endtimestamp:   now.AddDate(0, 1, 0),
+		},
+		{
+			Starttimestamp: now.AddDate(0, 1, 0),
+			Endtimestamp:   now.AddDate(0, 2, 0),
+		},
+		{
+			Starttimestamp: now.AddDate(0, 2, 0),
+			Endtimestamp:   now.AddDate(0, 3, 0),
+		},
+	}
+	expected := []StartEnd{
+		{
+			startt: now,                  // originally in the past, should start from now
+			endt:   data[0].Endtimestamp, // but the end should not change
+		},
+		{
+			startt: data[0].Endtimestamp, // continues from the previous end, no overlap
+			endt:   data[0].Endtimestamp.Add(getDurationUpTo(data, 1)),
+		},
+		{
+			startt: data[0].Endtimestamp.Add(getDurationUpTo(data, 1)),
+			endt:   data[0].Endtimestamp.Add(getDurationUpTo(data, 2)),
+		},
+		{
+			startt: data[0].Endtimestamp.Add(getDurationUpTo(data, 2)),
+			endt:   data[0].Endtimestamp.Add(getDurationUpTo(data, 3)),
+		},
+	}
+
+	fixupSubTime(data)
+	checkStartEndTimes("1", data, expected, t)
+
+	data = []*Row{ // now inserting at the end
 		{
 			Starttimestamp: now.Add(-(time.Hour * 24 * 7 * 2)),
 			Endtimestamp:   now.Add(-(time.Hour * 24 * 7 * 2)).AddDate(0, 1, 0),
@@ -72,17 +104,43 @@ func TestFixupSubTime(t *testing.T) {
 			Endtimestamp:   now.AddDate(0, 1, 0),
 		},
 	}
-	expected = []time.Time{
-		data[0].Endtimestamp,
-		data[0].Endtimestamp.Add(getDurationUpTo(data, 1)),
-		data[0].Endtimestamp.Add(getDurationUpTo(data, 2)),
-		data[0].Endtimestamp.Add(getDurationUpTo(data, 3)),
+	expected = []StartEnd{
+		{
+			startt: now,
+			endt:   data[0].Endtimestamp,
+		},
+		{
+			startt: data[0].Endtimestamp,
+			endt:   data[0].Endtimestamp.Add(getDurationUpTo(data, 1)),
+		},
+		{
+			startt: data[0].Endtimestamp.Add(getDurationUpTo(data, 1)),
+			endt:   data[0].Endtimestamp.Add(getDurationUpTo(data, 2)),
+		},
+		{
+			startt: data[0].Endtimestamp.Add(getDurationUpTo(data, 2)),
+			endt:   data[0].Endtimestamp.Add(getDurationUpTo(data, 3)),
+		},
 	}
 
 	fixupSubTime(data)
+	checkStartEndTimes("2", data, expected, t)
+
+}
+
+func checkOrderOfIDs(prefix string, data []*Row, expected []int64, t *testing.T) {
+	if len(expected) != len(data) {
+		t.Errorf("%v Expected the amount of input data to equal the expected data", prefix)
+		return
+	}
+
 	for k, v := range data {
-		if !v.Endtimestamp.Equal(expected[k]) {
-			t.Errorf("Expected2 date %+v, got %+v at index %v", expected[k], v.Endtimestamp, k)
+		if v.ID.Int64 != expected[k] {
+			t.Errorf(
+				"%v Expected the id at index %v to equal %v got %+v",
+				prefix, k, expected[k], v.ID.Int64,
+			)
+			return
 		}
 	}
 }
@@ -92,48 +150,59 @@ func TestAssembleSubs(t *testing.T) {
 		ID:   sql.NullInt64{1, true},
 		Tier: sql.NullInt64{1, true},
 	}
-	data := []*Row{
+	data := []*Row{ // just check if the sub was inserted at the end
 		{
+			ID:   sql.NullInt64{2, true},
 			Tier: sql.NullInt64{1, true},
 		},
 	}
+	expected := []int64{2, 1}
 
 	data = assembleSubs(data, newrow)
-	if len(data) != 2 && data[1].ID.Int64 != 1 {
-		t.Errorf("EVEN THE MOST BASIC SHIT FAILED %+v", data)
-	}
+	checkOrderOfIDs("1", data, expected, t)
 
-	data = []*Row{
+	data = []*Row{ // checking if the sub was inserted at the end again
 		{
+			ID:   sql.NullInt64{2, true},
 			Tier: sql.NullInt64{2, true},
 		},
 		{
+			ID:   sql.NullInt64{3, true},
 			Tier: sql.NullInt64{2, true},
 		},
 		{
+			ID:   sql.NullInt64{4, true},
 			Tier: sql.NullInt64{1, true},
 		},
 		{
+			ID:   sql.NullInt64{5, true},
 			Tier: sql.NullInt64{1, true},
 		},
 	}
+	expected = []int64{2, 3, 4, 5, 1}
 
 	data = assembleSubs(data, newrow)
-	if len(data) != 5 && data[4].ID.Int64 != 1 {
-		t.Errorf("was expecting the new row to be in position 4")
-	}
+	checkOrderOfIDs("2", data, expected, t)
 
-	newrow.Tier.Int64 = 2
-	data = data[0:3]
-	data = assembleSubs(data, newrow)
-	if len(data) != 5 && data[2].ID.Int64 != 1 {
-		t.Errorf("was expecting the new row to be in position 2")
+	// check if the new sub was inserted after subscriptions of the other tier2s,
+	// aka at 3rd position (index 2)
+	newrow = &Row{
+		ID:   sql.NullInt64{6, true},
+		Tier: sql.NullInt64{2, true},
 	}
+	expected = []int64{2, 3, 6, 4, 5, 1}
 
-	newrow.Tier.Int64 = 3
-	data = data[0:3]
 	data = assembleSubs(data, newrow)
-	if len(data) != 5 && data[0].ID.Int64 != 1 {
-		t.Errorf("was expecting the new row to be in position 0")
+	checkOrderOfIDs("3", data, expected, t)
+
+	// check if the new sub is inserted at the first place since its the highest
+	// tier
+	newrow = &Row{
+		ID:   sql.NullInt64{7, true},
+		Tier: sql.NullInt64{3, true},
 	}
+	expected = []int64{7, 2, 3, 6, 4, 5, 1}
+
+	data = assembleSubs(data, newrow)
+	checkOrderOfIDs("4", data, expected, t)
 }
